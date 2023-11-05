9f3e1a20ad4cce0540103d7ac92ff9635c8632dc
jackkslash <jackfaulknerbis@hotmail.co.uk>
2023-11-05 23:27:21 +0000

    Random cocktails on button press and load

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shapeflutter/models/cocktail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +32,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 194, 59, 59)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Cocktail Generator'),
     );
   }
 }
@@ -55,17 +60,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Cocktail> _rndCocktail = [];
+  String _cocktailName = "";
 
-  void _incrementCounter() {
+  Future getCocktail() async {
+    _rndCocktail.clear();
+    var response = await http.get(
+        Uri.parse('https://www.thecocktaildb.com/api/json/v1/1/random.php'));
+    var jsonData = jsonDecode(response.body);
+    print(jsonData);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      for (var eachDrink in jsonData['drinks']) {
+        final cocktail = Cocktail(
+            idDrink: eachDrink['idDrink'], drinkName: eachDrink['strDrink']);
+        _rndCocktail.add(cocktail);
+      }
+      _cocktailName = _rndCocktail[0].drinkName;
     });
+  }
+
+  @override
+  void initState() {
+    getCocktail();
+    super.initState();
   }
 
   @override
@@ -77,49 +95,61 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Randomly Chosen Drink:',
+              ),
+              Text(
+                '$_cocktailName',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FloatingActionButton.large(
+                onPressed: getCocktail,
+                tooltip: 'New Cocktail',
+                child:
+                    const Text("Random Cocktail", textAlign: TextAlign.center)),
+            FloatingActionButton.large(
+                onPressed: getCocktail,
+                tooltip: 'More Info',
+                child: const Text("More Info", textAlign: TextAlign.center)),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation
+            .centerDocked // This trailing comma makes auto-formatting nicer for build methods.
+
+        );
   }
 }
---
+++ lib/models/cocktail.dart
@@ -0,0 +1,9 @@
+class Cocktail {
+  final String idDrink;
+  final String drinkName;
+
+  const Cocktail({
+    required this.idDrink,
+    required this.drinkName,
+  });
+}
